4043879b968fe043ca72fbeda984491252517252
bcasias <bcasias@mymail.mines.edu>
2013-04-24 19:48:01 -0600

    Added land claiming

--- src/buildings/City.java
+++ src/buildings/City.java
@@ -1,14 +1,20 @@
 package buildings;
 
+import java.util.ArrayList;
+
 public class City extends Building {
 	public enum CityType {
 		VILLAGE, TOWN, CITY
 	}
+	private ArrayList<Integer> landOwned;
+	private int locationX;
+	private int locationY;
 
 	private int pop;
 
 	public City() {
 		super(5, 5, 5);
+		landOwned = new ArrayList<Integer>();
 		pop = 1;
 	}
 
@@ -23,6 +29,33 @@ public class City extends Building {
 			return CityType.TOWN;
 		else
 			return CityType.VILLAGE;
+	}
+	
+	public void placeCity(int x, int y) {
+		locationX = x;
+		locationY = y;
+	}
+	
+	public boolean addLand(int x, int y) {
+		int location = sticksAndStones.GameManager.calculateIndex(x, y);
+		for (int land : landOwned) {
+			if (land == location) {
+				return false;
+			} else if (land == location - 1) {
+				landOwned.add(location - 1);
+				return true;
+			} else if (land == location + 1) {
+				landOwned.add(location + 1);
+				return true;
+			} else if (land == location - sticksAndStones.GameManager.getBoardSizeY()) {
+				landOwned.add(location - sticksAndStones.GameManager.getBoardSizeY());
+				return true;
+			} else if (land == location + sticksAndStones.GameManager.getBoardSizeY()) {
+				landOwned.add(location + sticksAndStones.GameManager.getBoardSizeY());
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public int getPop() {
