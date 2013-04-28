49bf2c15ea3ee5bca0e1e9b0e6ebb5185eaf5a89
Ryan Stauffer <rstauffe@mymail.mines.edu>
2013-04-28 14:57:15 -0600

    resolved compiler errors

--- src/buildings/ImprovementBuilding.java
+++ src/buildings/ImprovementBuilding.java
@@ -13,16 +13,6 @@ public class ImprovementBuilding extends Building {
 		super(locx, locy, wood, stone, gold);
 		this.buildingType = buildingType;
 		
-<<<<<<< HEAD
-		switch(buildingType)
-		{
-			case FARM:
-			case BARRACK:
-			case MINE:
-			case SAWMILL:
-			case TRADINGPOST:
-		}
-=======
 		try 
 		 {
 			switch(buildingType)
@@ -30,13 +20,12 @@ public class ImprovementBuilding extends Building {
 				case FARM: 			image = ImageIO.read(new File("Textures/Building/Farm")); break;
 				case BARRACK:		image = ImageIO.read(new File("Textures/Building/Barrack")); break;
 				case MINE:			image = ImageIO.read(new File("Textures/Building/Mine")); break;
-				case SAWMIL:		image = ImageIO.read(new File("Textures/Building/SawMill")); break;
+				case SAWMILL:		image = ImageIO.read(new File("Textures/Building/SawMill")); break;
 				case TRADINGPOST:	image = ImageIO.read(new File("Textures/Building/TradingPost")); break;
 			}
 	     } catch (IOException ex) {
 	            // handle exception...
 	     }
->>>>>>> cdfcd3e4a58df7647ea962a1948c778376b1f26f
 	}
 	
 	public boolean hasResource()
